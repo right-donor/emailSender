6b81eccfe46d9fb79e5c09d90821e31df1c306e4
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2019-10-22 20:17:21 -0500

    Created E-Mail Service using Mailgun

---
+++ emailSender/handler.rb
@@ -0,0 +1,30 @@
+require 'json'
+require 'rubygems'
+require 'mailgun-ruby'
+
+mg_client = Mailgun::Client.new ENV.API_KEY
+
+def hello(event:, context:)
+  if event["to"] == "" || event["subject"] == "" || event["text"] == ""
+    {
+      statusCode: 200,
+      body: {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event
+    }.to_json
+  else
+    send_email(event["to"],event["subject"],event["text"])
+  end
+end
+
+
+def send_email(to, subject, text) {
+  message_params =  { from: 'bob@'+ENV.DOMAIN,
+                    to:   to,
+                    subject: subject,
+                    text:    text
+                  }
+
+  # Send your message through the client
+  return mg_client.send_message ENV.DOMAIN, message_params
+}
---
+++ emailSender/ruby/2.5.0/gems/mailgun-ruby-1.1.11/lib/mailgun/version.rb
@@ -0,0 +1,4 @@
+# It's the version. Yeay!
+module Mailgun
+  VERSION = '1.1.11'
+end
