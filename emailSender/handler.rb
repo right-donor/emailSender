498e16a4cab2239e1a3057ed8c2383ef5786d40b
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2019-10-22 21:33:17 -0500

    Removed old code
    Updated to new code

--- emailSender/handler.rb
+++ emailSender/handler.rb
@@ -1,11 +1,23 @@
+# __   __  _______  ___   ___      _______  _______  ______    ______    _______  ___     
+# |  |_|  ||   _   ||   | |   |    |  _    ||   _   ||    _ |  |    _ |  |       ||   |    
+# |       ||  |_|  ||   | |   |    | |_|   ||  |_|  ||   | ||  |   | ||  |    ___||   |    
+# |       ||       ||   | |   |    |       ||       ||   |_||_ |   |_||_ |   |___ |   |    
+# |       ||       ||   | |   |___ |  _   | |       ||    __  ||    __  ||    ___||   |___ 
+# | ||_|| ||   _   ||   | |       || |_|   ||   _   ||   |  | ||   |  | ||   |___ |       |
+# |_|   |_||__| |__||___| |_______||_______||__| |__||___|  |_||___|  |_||_______||_______|
+# Made by : A01334390
+# Creation Date: October 22nd 2019
+
 require 'json'
 require 'rubygems'
 require 'mailgun-ruby'
 
-mg_client = Mailgun::Client.new ENV["API_KEY"]
-
+# Lambda function main executer
+# Params:
+# +event:: POST Event Object
+# +context:: Execution context
 def hello(event:, context:)
-  if event["to"] == "" || event["subject"] == "" || event["text"] == ""
+  if event["to"] == "" || event["subject"] == "" || event["text"] == "" || event["first"] == "" || event["second"] == ""
     {
       statusCode: 200,
       body: {
@@ -13,18 +25,23 @@ def hello(event:, context:)
         input: event
     }.to_json}
   else
-    send_email(event["to"],event["subject"],event["text"])
+    send_email(event["to"],event["first"],event["second"],event["subject"],event["text"])
   end
 end
 
-
-def send_email(to, subject, text)
-  message_params =  { from: 'bob@'+ENV.DOMAIN,
-                    to:   to,
-                    subject: subject,
-                    text:    text
-                  }
-
-  # Send your message through the client
-  return mg_client.send_message ENV["DOMAIN"], message_params
+# Sends an email to the desired recipient
+# Params:
+# +to:: Email of the recipient
+# +first_name: First Name of the recipient
+# +second_name: Second Name of the recipient
+# +subject:: Subject of the email to send
+# +text:: Email text
+def send_email(to, first_name, second_name, subject, text)
+  mg_client = Mailgun::Client.new ENV["API_KEY"]
+  mb_obj = Mailgun::MessageBuilder.new
+  mb_obj.set_from_address("bob@"+ENV["DOMAIN"],{"first" => "Right", "last" => "Donor"})
+  mb_obj.add_recipient(:to,to,{"first" => first_name, "last" => second_name})
+  mb_obj.set_subject(subject)
+  mb_obj.set_html_body("<html><body><h1> Hello from Right Donor </h1> <p>"+text+"</p></body></html>")
+  return mg_client.send_message(ENV["DOMAIN"],mb_obj)
 end
